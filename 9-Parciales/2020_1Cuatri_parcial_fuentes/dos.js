3df4f71566f462b028b07ad7d9e39879ee26a9a9
pacugliari <pacugliari@hotmail.com>
2022-02-24 11:49:59 -0300

    Clase 11-Ejercicios tipo parcial

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,4 +1,105 @@
+/*
+Enunciado:
+
+Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
+Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
+a) El importe total a pagar , bruto sin descuento y...
+b) el importe total a pagar con descuento(solo si corresponde)
+d) Informar el tipo con mas cantidad de bolsas.
+f) El tipo mas caro
+*/
 function mostrar()
 {
-  alert("dos");
+  var tipo;
+  var cantidad;
+  var precio;
+  var respuesta="si";
+  var cantidadBolsas=0;
+  var precioTotal=0;
+  var descuento;
+  var precioDescuento;
+  var cantidadCal=0;
+  var cantidadCemento=0;
+  var cantidadArena=0;
+  var mensaje="";
+  var esPrimero = true;
+  var maximoTipo;
+  var maximoPrecio;
+
+  while(respuesta=="si" || respuesta=="SI"){
+    
+    tipo = prompt("Ingrese el tipo de producto");
+    while(tipo != "arena" && tipo!= "cal" && tipo!= "cemento"){
+      tipo = prompt("Error en tipo,Ingrese el tipo de producto");
+    }
+
+    cantidad = prompt("Ingrese la cantidad de producto");
+    while(cantidad <0){
+      cantidad = prompt("Error en cantidad,Ingrese la cantidad de producto");
+    }
+
+    precio= prompt("Ingrese el precio del producto");
+    while(precio < 0){
+      precio= prompt("Error en precio,Ingrese el precio del producto");
+    }
+
+    console.log(tipo,cantidad,precio)
+    cantidad=parseInt(cantidad);
+    precio=parseFloat(precio);
+
+    cantidadBolsas+=cantidad;
+    precioTotal+=precio*cantidad;
+
+    if(maximoPrecio < precio || esPrimero){
+      maximoTipo = tipo;
+      maximoPrecio = precio;
+      esPrimero=false;
+    }
+
+    switch(tipo){
+      case "arena":
+        cantidadArena+=cantidad;
+      break;
+      case "cal":
+        cantidadCal+=cantidad;
+      break;
+      case "cemento":
+        cantidadCemento+=cantidad;
+      break;
+    }
+
+    do{
+      respuesta = prompt("Desea seguir agregando productos ? SI/NO")
+    }while(respuesta != "si" && respuesta !="SI" && respuesta!="no" && respuesta!="NO")
+  }
+
+  if(cantidadBolsas > 30){
+    descuento=0.75;
+  }else if(cantidadBolsas >10){
+    descuento=0.85;
+  }else
+    descuento=1;
+
+  precioDescuento=precioTotal*descuento;
+
+  mensaje+="a)Importe total: "+precioTotal+"<br>";
+  mensaje+="b)Importe descuento: "+precioDescuento+"<br>";
+
+  mensaje+="c)El tipo con mas cantidad es";
+  if(cantidadArena > cantidadCal && cantidadArena > cantidadCemento){
+    mensaje+=" arena <br>";
+  }else if(cantidadCal > cantidadArena && cantidadCal>cantidadCemento){
+    mensaje+=" cal <br>";
+  }else{
+    mensaje+=" cemento <br>";
+  }
+
+  mensaje+="d)El tipo mas caro es: "+maximoTipo+"<br>";
+  document.write(mensaje);
+
 }
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,109 @@
+/*
+Enunciado:
+
+Debemos realizar la carga de 5(cinco) productos de prevención de contagio,
+de cada una debo obtener los siguientes datos:
+el tipo (validar "barbijo" , "jabón" o "alcohol") ,
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+la Marca y el fabricante.
+Se debe Informar al usuario lo siguiente:
+a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+b) Del tipo con mas unidades, el promedio por compra
+c) Cuántas unidades de jabones hay en total*/
+
+
 
 function mostrar()
 {
-	alert("uno");
+	var tipo;
+	var precio;
+	var cantidad;
+	var marca;
+	var fabricante;
+	var minimoPrecio;
+	var minimoCantidad;
+	var minimoFabricante;
+	var esPrimerIngresoAlcohol=true;
+	var mayorTipo;
+	var mensaje="";
+	var promedio;
+
+	var cantidadJabon=0;
+	var cantidadBarbijo=0;
+	var cantidadAlcohol=0;
+	var contadorJabon=0;
+	var contadorBarbijo=0;
+	var contadorAlcohol=0;
+	
+
+	for(var i=1;i<4;i++){
+		precio=0;
+		cantidad=0;
+		tipo="";
+
+		while(tipo != "barbijo" && tipo!="jabon" && tipo!="alcohol"){
+			tipo = prompt ("Ingrese el tipo,(validar barbijo , jabón o alcohol)");
+		}
+	
+		while(precio <100 || precio >300){
+			precio = prompt("Ingrese el precio entre 100 y 300");
+		}
+	
+		while(cantidad<1 || cantidad >1000){
+			cantidad=prompt("Ingrese la cantidad de unidades entre 1 y 1000");
+		}
+		
+		cantidad=parseInt(cantidad);
+		precio=parseFloat(precio);
+
+		marca = prompt("Ingrese la marca");
+		fabricante = prompt ("Ingrese la fabricante");
+
+		switch(tipo){
+			case "alcohol":
+				cantidadAlcohol+=cantidad;
+				contadorAlcohol++;
+				 if(precio < minimoPrecio || esPrimerIngresoAlcohol){
+					minimoPrecio=precio;
+					minimoCantidad=cantidad;
+					minimoFabricante=fabricante;
+					esPrimerIngresoAlcohol=false;
+				}
+			break;
+			case "barbijo":
+				cantidadBarbijo+=cantidad;
+				contadorBarbijo++;
+			break;
+			case "jabon":
+				cantidadJabon+=cantidad;
+				contadorJabon++;
+			break;
+		}
+
+		if(cantidadAlcohol>cantidadBarbijo && cantidadAlcohol>cantidadJabon){
+			mayorTipo="alcohol";
+			promedio = cantidadAlcohol/contadorAlcohol;
+		}else if (cantidadBarbijo> cantidadAlcohol && cantidadBarbijo>cantidadJabon){
+			mayorTipo="barbijo";
+			promedio = cantidadBarbijo/contadorBarbijo;
+		}else{
+			mayorTipo="jabon";
+			promedio = cantidadJabon/contadorJabon;
+		}
+
+		console.log(tipo,precio,cantidad,marca,fabricante);
+		
+	}
+
+	//a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+	//b) Del tipo con mas unidades, el promedio por compra
+	//c) Cuántas unidades de jabones hay en total
+	mensaje+="<br>"
+	mensaje+="a) El mas barato de los alcoholes cuesta: "+minimoPrecio+" la cantidad de unidades fueron: "+minimoCantidad+" y el fabricante: "+minimoFabricante+"<br>";
+	mensaje+="b) El tipo con mas unidades es: "+mayorTipo+" y el promedio por compra es: "+promedio+ "<br>";
+	mensaje+="c) Cantidad de jabones en total es: "+cantidadJabon+"<br>";
+
+	document.write(mensaje);
+
 }
